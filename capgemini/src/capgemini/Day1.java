c1ad536446a567b0ac901271e296c91bcba61d2b
Asus <Asus@Artz>
2024-07-26 15:57:31 +0530

    hello

---
+++ capgemini/src/capgemini/Day1.java
@@ -0,0 +1,18 @@
+package capgemini;
+import java.util.*;
+public class Day1 {
+	public static void main(String[] args) {
+     int a,b,c;
+     Scanner sc = new Scanner(System.in);
+     System.out.println("give value g");
+     System.out.println("give value d");
+     int d= sc.nextInt();
+     int g=sc.nextInt();
+     a=12;
+     b=13;
+     String name= sc.nextLine();
+     System.out.println("give name");
+     System.out.println(a+b+ " - the addition");
+     System.out.println(g-d+"- the sub");
+	}
+}
